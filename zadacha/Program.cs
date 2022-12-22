663fb14e16b7a9ac0b4ad15f351ce45f3ff0504d
Lena <len.chaika2011@yandex.ru>
2022-12-22 23:38:21 +0300

    zadacha

---
+++ zadacha/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Input number: ");
+int i = int.Parse(Console.ReadLine());
+
+switch (i)
+{
+    case 1: Console.WriteLine ("Mon"); break;
+    case 2: Console.WriteLine ("Tue"); break;
+    case 3: Console.WriteLine ("Wed"); break;
+    case 4: Console.WriteLine ("Thu"); break;
+    case 5: Console.WriteLine ("Fri"); break;
+    case 6: Console.WriteLine ("Sat"); break;
+    case 7: Console.WriteLine ("Sun"); break;
+    default: Console.WriteLine ("It's not a weak day"); break;
+}
