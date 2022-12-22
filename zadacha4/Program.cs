ea4609da3009b6979c1975385039d5d50360748e
Lena <len.chaika2011@yandex.ru>
2022-12-23 00:24:20 +0300

    zadacha4

--- zadacha4/Program.cs
+++ zadacha4/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.44 5 78 -> 78
+
+Console.WriteLine("введите 3 числа: ");
+
+int number_a = int.Parse(Console.ReadLine());
+int number_b = int.Parse(Console.ReadLine());
+int number_c = int.Parse(Console.ReadLine());
+
+int max = number_a;
+
+if (number_b > max)
+{
+    max = number_b;
+}
+if (number_c > max)
+{
+    max = number_c;
+}
+Console.Write($"max number = {max}");
