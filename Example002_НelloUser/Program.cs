15d902ddb7c9f01d159845d0988df72417cd9951
Lena <len.chaika2011@yandex.ru>
2022-12-22 13:47:08 +0300

    example002

--- Example002_НelloUser/Program.cs
+++ Example002_НelloUser/Program.cs
@@ -1,4 +1,4 @@
 ﻿Console.Write("введите ваше имя ");
-String username = Console.ReadLine( );
+string username = Console.ReadLine();
 Console.Write("привет, ");
 Console.Write(username);
---
+++ уexample003/Program.cs
@@ -0,0 +1,4 @@
+﻿numberA = 3;
+int numberB = 5;
+int result = numberB + numberA;
+Console.WriteLine(numberA + numberB);
