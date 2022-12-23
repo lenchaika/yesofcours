8124d604d39dec66925aa5724ac8fdd63044b3aa
Lena <len.chaika2011@yandex.ru>
2022-12-24 00:57:46 +0300

    seminar2

--- seminar2/Program.cs
+++ seminar2/Program.cs
@@ -22,6 +22,9 @@
 // a = value div 100
 // b = value (value div 10) mod 10 
 // c = value mod 10
+// -214: Abs(-214) = 214 --> 24
+//
+//
 
 // Метод который отковырять вторую цифру
 
@@ -33,17 +36,55 @@ int GetNumber(int value)
 
     return a * 10 + c;
 }
-    System.Console.WriteLine(GetNumber(176));
-
-
+    
 
 // 2.1.Метод который -ввод числа
+
 int GetNumberFromUser()
 {
+    Console.Write("Введите число от 100 до 999: ");
     int temp = int.Parse(Console.ReadLine()!);
     return temp;
 }
 // 2.2 Метод который -проверка числа на валидность
+bool IsValid(int number)
+ 
+{
+    return number >= 100 && number <= 999;
+    //if (number >= 100)
+    //{
+     //   if (number <= 999); (можно так или как выше)
+     //   {
+     //       return true;
+       // }
+
+   // }
+   // return false; 
+}
 // 2.3. Метод который -отковырять вторую цифру !
 // 2.4 Метод который -распечатать результат
+
+string GetText(int input, int output)
+{
+    return input + " --> " + output;
+}
 // 2.5. Метод который -декаратор для вывода
+
+string Decorator(string text)
+{
+    Console.Clear();
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(text);
+    return text;
+}
+
+int value = GetNumberFromUser();
+if (IsValid(value))
+{
+    int result = GetNumber(value);
+    System.Console.WriteLine(Decorator(GetText(value, result)));
+}
+else
+{
+    System.Console.WriteLine("число кривое");
+}
