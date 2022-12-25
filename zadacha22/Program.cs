170efeeca302f40ed0e6bfafcd48d5060e5aae13
Lena <len.chaika2011@yandex.ru>
2022-12-25 22:41:30 +0300

    zadacha22

---
+++ zadacha22/Program.cs
@@ -0,0 +1,19 @@
+﻿// программа которая принимает на вход число N и выдает таблицу квадратов чисел от 1 до N
+
+Console.Write("введите число : ");
+int N = int.Parse(Console.ReadLine());
+int count = 1;
+while(count <= N);
+{
+   // Concole.WriteLine(Math.Pow(count, 2));
+    Console.WriteLine($"{Math.Pow(count, 2)}");
+    count++;
+}
+
+//Console.Write("введите число : ");
+//int N = int.Parse(Console.ReadLine());
+//for(int I = 1; I<=N; I++)
+//{
+   // if (I--N){Console.Write($"{Math.Pow(I, 2)}");}
+   //else Console.Write($"{Math.Pow(I, 2)}, ");
+//}
