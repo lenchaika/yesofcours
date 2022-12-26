61c0fc595dce94873c6ed4ecaef07d81cd943354
Lena <len.chaika2011@yandex.ru>
2022-12-26 14:34:27 +0300

    zadacha22

--- zadacha22/Program.cs
+++ zadacha22/Program.cs
@@ -3,7 +3,7 @@
 Console.Write("введите число : ");
 int N = int.Parse(Console.ReadLine());
 int count = 1;
-while(count <= N);
+while(count <= N)
 {
    // Concole.WriteLine(Math.Pow(count, 2));
     Console.WriteLine($"{Math.Pow(count, 2)}");
@@ -14,6 +14,6 @@ while(count <= N);
 //int N = int.Parse(Console.ReadLine());
 //for(int I = 1; I<=N; I++)
 //{
-   // if (I--N){Console.Write($"{Math.Pow(I, 2)}");}
-   //else Console.Write($"{Math.Pow(I, 2)}, ");
+// if (I--N){Console.Write($"{Math.Pow(I, 2)}");}
+//else Console.Write($"{Math.Pow(I, 2)}, ");
 //}
